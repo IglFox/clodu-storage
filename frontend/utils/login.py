9e50d58de750bebc74ef69db9e1558f912e28113
IglFox <iglfox@yandex.ru>
2026-04-12 02:50:53 +0300

    core: creating auth ui

--- frontend/utils/login.py
+++ frontend/utils/login.py
@@ -1,18 +1,40 @@
 import streamlit as st
+from schemas.Schemas import User
+import requests
+from logging import get_logger
 
 
-def check_login():
-    if "authenticated" not in st.session_state:
-        st.session_state.authenticated = False
+_logger = get_logger(__name__)
 
-    if not st.session_state.authenticated:
-        with st.form("login"):
-            pwd = st.text_input("Пароль", type="password")
-            if st.form_submit_button("Войти"):
-                if pwd == "secret":
-                    st.session_state.authenticated = True
-                    st.rerun()
-                else:
-                    st.error("Неверный пароль")
-        return False
-    return True
+
+class LoginManager:
+    def __init__(self):
+        if "authenticated" not in st.session_state:
+            st.session_state.authenticated = False
+
+    def is_user_have_account(self, username: str) -> bool:
+        try:
+            request = requests.get(f"http://auth/users/{username}")
+        except Exception:
+            return False
+        return request.status_code == 200
+
+    def login(self, username: str, password: str) -> str:
+        if st.session_state.authenticated:
+            return "already_authenticated"
+
+        if self.is_user_have_account(username):
+            return "account_exists"
+
+        try:
+            request = requests.post(
+                "http://auth/login", json={"username": username, "password": password}
+            )
+        except Exception:
+            return "failed"
+
+        if request.status_code == 200:
+            st.session_state.authenticated = True
+            return "sucess"
+
+        return "failed"
